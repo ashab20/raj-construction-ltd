347f7f713064a8f6ca015e0910df0864b94042cd
Md Rabib Hasan <mdrabib@github.com>
2022-11-07 22:04:37 +0600

    changes public directory

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,15 +7,15 @@
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('public/assets/images/favicon.ico') }}">
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
         <!-- third party css -->
-        <link href="{{ asset('public/assets/css/vendor/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/vendor/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
         <!-- third party css end -->
 
        <!-- App css -->
-       <link href="{{ asset('public/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-       <link href="{{ asset('public/assets/css/app-modern.min.css') }}" rel="stylesheet" type="text/css" id="light-style" />
-       <link href="{{ asset('public/assets/css/app-modern-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style" />
+       <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+       <link href="{{ asset('assets/css/app-modern.min.css') }}" rel="stylesheet" type="text/css" id="light-style" />
+       <link href="{{ asset('assets/css/app-modern-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style" />
         
     </head>
