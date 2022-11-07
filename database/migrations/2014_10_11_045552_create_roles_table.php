53a53517188fd8050dedf59c660d7efe5d0048b6
ashab20 <ashabuddin20@hotmail.com>
2022-11-07 12:18:41 +0600

    update roles

---
+++ database/migrations/2014_10_11_045552_create_roles_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role')->unique()->index();
+            $table->string('identify')->index();
+            $table->integer('status')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        DB::table('roles')->insert([
+            [
+                'role'=>'Admin',
+                'identify'=>'ADMIN',
+                'created_at'=>Carbon::now(),
+            ],
+            [
+                'role'=>'Moderator',
+                'identify'=>'MODERATOR',
+                'created_at'=>Carbon::now(),
+            ],
+            [
+                'role'=>'Admin',
+                'identify'=>'ADMIN',
+                'created_at'=>Carbon::now(),
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
