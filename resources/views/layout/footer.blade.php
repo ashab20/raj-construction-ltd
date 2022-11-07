cd12a714ad883e3dc0656942c55f872a64a19d41
ashab20 <ashabuddin20@hotmail.com>
2022-11-07 11:12:04 +0600

    update view

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,18 @@
+
+
+        <!-- bundle -->
+        <script src="{{ asset('public/assets/js/vendor.min.js') }}"></script>
+        <script src="{{ asset('public/assets/js/app.min.js') }}"></script>
+
+        <!-- third party js -->
+        <script src="{{ asset('public/assets/js/vendor/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('public/assets/js/vendor/jquery-jvectormap-1.2.2.min.js') }}"></script>
+        <script src="{{ asset('public/assets/js/vendor/jquery-jvectormap-world-mill-en.js') }}"></script>
+        <!-- third party js ends -->
+
+        <!-- demo app -->
+        <script src="{{ asset('public/assets/js/pages/demo.dashboard.js') }}"></script>
+        <!-- end demo js-->
+        
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('register', function () {
+    return view('profile.register');
+});
+
