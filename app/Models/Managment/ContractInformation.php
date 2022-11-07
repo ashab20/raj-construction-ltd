9f958762437d1b107aba58d1cf2ee770bbdc272a
ashab20 <ashabuddin20@hotmail.com>
2022-11-07 11:30:28 +0600

    database migrations

---
+++ app/Models/Managment/ContractInformation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Managment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContractInformation extends Model
+{
+    use HasFactory;
+}
