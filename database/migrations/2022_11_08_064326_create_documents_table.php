a6b7839791a0cc852c909616631f77e500b9d7ea
Md Jahid Rony <jahidrony@github.com>
2022-11-08 13:25:46 +0600

    land

--- database/migrations/2022_11_08_064326_create_documents_table.php
+++ database/migrations/2022_11_08_064326_create_documents_table.php
@@ -16,16 +16,18 @@ return new class extends Migration
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->unsignedBigInteger('land_owner');
+            $table->unsignedBigInteger('user_id')->foreign(users)->references('id')->on('users')->onDelete('cascade')->change();
             $table->integer('squire_feet')->default(1);
             $table->string('house_no');
             $table->string('block');
             $table->integer('road_no')->default(1);
             $table->string('address');
-            $table->unsignedBigInteger('documents_id');
-            $table->unsignedBigInteger('design_id');
-            $table->unsignedBigInteger('item_id');
-            
+            $table->unsignedBigInteger('documents_id')->foreign()->references('id')->on('documents')->onDelete('cascade')->change();
+            $table->unsignedBigInteger('design_id')->foreign()->references('id')->on('designs')->onDelete('cascade')->change();
+            $table->decimal('total_budget',12,2);
+            $table->decimal('total_cost',12,2);
+            $table->integer('squire_feet')->default(1);
+
 
                 //default
             $table->integer('status')->default(1);
