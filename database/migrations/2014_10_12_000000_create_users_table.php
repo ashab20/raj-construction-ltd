075327c7ec5da25fef535676ff20d0438316643f
ashab20 <ashabuddin20@hotmail.com>
2022-11-08 13:29:14 +0600

    database changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('role_id');
+            $table->string('language')->default('en');
             $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
