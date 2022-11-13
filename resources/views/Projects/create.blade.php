27e74c4f3e1b33ab93bab729e46e6d5aae5bff0a
Ashab Uddin <ashabuddin20@hotmail.com>
2022-11-14 01:56:21 +0600

    nothing

--- resources/views/Projects/create.blade.php
+++ resources/views/Projects/create.blade.php
@@ -29,12 +29,14 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <form action="">
+                        <form action="{{route('project.store')}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('post')
                          
                         <div class="row">                            
                                 <div class="col-xl-6 mb-3">
                                     <label for="projectname" class="form-label">{{__('Project Name')}}</label>
-                                    <input type="text" id="projectname" class="form-control" placeholder="Enter project name" name="projectNameInputField">
+                                    <input type="text" id="projectname" class="form-control" placeholder="Enter project name" name="projectNameInputField" value="{{ old('projectNameInputField')}}">
                                 </div>
 
                                 <div class="mb-0 col-xl-6 col-6">
@@ -81,12 +83,14 @@
                                     <label class="form-label">{{__('Start Date')}}</label>
                                     <input type="text" class="form-control" data-provide="datepicker" data-date-container="#datepicker1" data-date-format="d-M-yyyy" data-date-autoclose="true"
                                     name="parojectStarDate"
+                                    value="{{ old('parojectStarDate')}}"
                                     >
                                 </div>
                                 <div class="col-xl-6 position-relative" id="datepicker2">
                                     <label class="form-label">{{__('Due Date')}}</label>
                                     <input type="text" class="form-control" data-provide="datepicker" 
                                     name="parojectEndDate"
+                                    value="{{ old('parojectEndDate')}}"
                                     data-date-container="#datepicker2" data-date-format="d-M-yyyy" data-date-autoclose="true">
                                 </div>
                             </div>
@@ -94,19 +98,21 @@
                                 <div class="col-xl-6 mb-3">
                                     <label for="project-overview" class="form-label">{{__('Overview')}}</label>
                                     <textarea class="form-control" id="project-overview"
-                                    name="projectOverview" rows="5" placeholder="Enter some brief about project.."></textarea>
+                                    name="projectOverview" rows="5" placeholder="Enter some brief about project..">{{old('projectOverview')}}</textarea>
                                 </div>
                                  <!-- Date View -->
                                  <div class="col-xl-6">
                                     
                                     <div class="mb-3">
                                         <label for="project-budget" class="form-label">{{__('Budget')}}</label>
-                                        <input type="text" id="project-budget" class="form-control" name="totalBudget" placeholder="Enter project budget">
+                                        <input type="text" id="project-budget" class="form-control" name="totalBudget" placeholder="Enter project budget"
+                                        value="{{ old('totalBudget')}}">
                                     </div>
                                     <div class="mb-3 mt-3 mt-xl-0">
                                         <label for="projectname" class="mb-0">Project Image</label>
                                         
-                                            <input class="form-control" type="file" id="inputGroupFile04" name="projectImage">
+                                            <input class="form-control" type="file" id="inputGroupFile04" name="projectImage"
+                                            value="{{old('projectImage')}}">
                                             <p class="text-muted font-14">Recommended thumbnail size 800x400 (px).</p>
                                     </div>
                                  </div>
@@ -118,7 +124,7 @@
                             <div class="row">
                                 <div class="mb-3 col-xl-4 form-row">
                                     <label for="squireFeet" class="form-label">{{__('Plot Area')}} :</label>
-                                    <input type="text" id="squireFeet" name="ploatArea" class="form-control">
+                                    <input type="number" id="squireFeet" name="ploatArea" class="form-control" value="{{old('ploatArea')}}">
                                     <select name="ploatAreaCounter" id="" class="form-control">
                                         <option value="" selected>Squire Feet</option>
                                         <option value="">Miter</option>
@@ -126,7 +132,7 @@
                                 </div>
                                 <div class="mb-3 col-xl-4 form-row">
                                     <label for="squireFeet" class="form-label">{{__('Building Area')}} :</label>
-                                    <input type="text" id="squireFeet" name="BuildingArea" class="form-control">
+                                    <input type="number" id="squireFeet" name="BuildingArea" class="form-control" value="{{old('BuildingArea')}}">
                                     <select name="BuildingAreaCounter" id="" class="form-control">
                                         <option value="Squire Feet" selected>Squire Feet</option>
                                         <option value="Miter">Miter</option>
@@ -134,7 +140,7 @@
                                 </div>
                                 <div class="mb-3 col-xl-4 form-row">
                                     <label for="squireFeet" class="form-label">{{__('Building Height')}} :</label>
-                                    <input type="text" id="squireFeet" name="BuildingHeight" class="form-control">
+                                    <input type="number" id="squireFeet" name="BuildingHeight" class="form-control" value="{{old('BuildingHeight')}}">
                                     <select name="BuildingHeightCounter" id="" class="form-control">
                                         <option value="Miter" selected>Miter</option>
                                         <option value="Squire Feet">Squire Feet</option>
@@ -144,7 +150,7 @@
                             <div class="row">                      
                                 <div class="mb-3 col-xl-4 form-row">
                                     <label for="houseNo" class="form-label">House No.</label>
-                                    <input type="text" id="houseNo" class="form-control" name="houseNo">
+                                    <input type="text" id="houseNo" class="form-control" name="houseNo" value="{{old('houseNo')}}">
                                 </div>
                                 <div class="mb-3 col-xl-4 form-row">
                                     <label for="block" class="form-label">Block</label>
