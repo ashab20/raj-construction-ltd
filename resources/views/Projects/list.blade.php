928a35d78ef629d12d23d6cd106dcdf4798d08d6
harunurR <harunurrashid97.ctg@gmail.com>
2022-11-16 13:16:26 +0600

    project images

--- resources/views/Projects/list.blade.php
+++ resources/views/Projects/list.blade.php
@@ -48,125 +48,112 @@
 
         <div class="row">
             @forelse($projects as $project)
-           
-            <div class="col-lg-6 col-xxl-3">
+            <div class="col-md-6 col-xxl-3">
+                <!-- project card -->
                 <div class="card d-block">
-                    <div class="card-body">
-                        <div class="dropdown card-widgets">
-                            <a href="#" class="dropdown-toggle arrow-none" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="dripicons-dots-3"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item"><i class="mdi mdi-pencil me-1"></i>Edit</a>
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item"><i class="mdi mdi-delete me-1"></i>Delete</a>
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item"><i class="mdi mdi-email-outline me-1"></i>Invite</a>
-                                <!-- item-->
-                                <a href="javascript:void(0);" class="dropdown-item"><i class="mdi mdi-exit-to-app me-1"></i>Leave</a>
-                            </div>
-                        </div>
-                        <!-- project title-->
-                        <h4 class="mt-0">
-                            <a href="apps-projects-details.html" class="text-title">
-                                {{$project->project_name}}
-                            </a>
-                        </h4>
+                    <!-- project-thumbnail -->
+                    <img class="card-img-top" src="{{asset('/uploads/projects'.$project->project_image)}}" alt="project image cap">
+                    <div class="card-img-overlay">
                         @if($project->stage->stage === 'Pending')
                         <div class="badge bg-secondary text-light p-1">{{$project->stage->stage}}</div>
                         @else
                         <div class="badge bg-success mb-3">{{$project->stage->stage}}</div>
 
                         @endif
-                        <p class="text-muted font-13 mb-3">
-                        {{Str::substr($project->project_overview, 0, 100);}}
-                       
-                        <a href="javascript:void(0);" class="fw-bold text-muted">view more</a>
-                        </p>
+                    </div>
 
-                        <!-- project detail-->
-                        <p class="mb-1">
-                            <span class="pe-2 text-nowrap mb-2 d-inline-block">
-                                <i class="mdi mdi-format-list-bulleted-type text-muted"></i>
-                                <b>21</b> Tasks
-                            </span>
-                            <span class="text-nowrap mb-2 d-inline-block">
-                                <i class="mdi mdi-comment-multiple-outline text-muted"></i>
-                                <b>741</b> Comments
-                            </span>
-                        </p>
-                        <div id="tooltip-container">
-                            <a href="javascript:void(0);" data-bs-container="#tooltip-container" data-bs-toggle="tooltip" data-bs-placement="top" title="Mat Helme" class="d-inline-block">
-                                <img src="assets/images/users/avatar-6.jpg" class="rounded-circle avatar-xs" alt="friend">
-                            </a>
-
-                            <a href="javascript:void(0);" data-bs-container="#tooltip-container" data-bs-toggle="tooltip" data-bs-placement="top" title="Michael Zenaty" class="d-inline-block">
-                                <img src="assets/images/users/avatar-7.jpg" class="rounded-circle avatar-xs" alt="friend">
-                            </a>
-
-                            <a href="javascript:void(0);" data-bs-container="#tooltip-container" data-bs-toggle="tooltip" data-bs-placement="top" title="James Anderson" class="d-inline-block">
-                                <img src="assets/images/users/avatar-8.jpg" class="rounded-circle avatar-xs" alt="friend">
-                            </a>
-                            <a href="javascript:void(0);" class="d-inline-block text-muted fw-bold ms-2">
-                                +7 more
-                            </a>
-                        </div>
-                    </div> <!-- end card-body-->
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item p-3">
-                            <!-- project progress-->
-                            <p class="mb-2 fw-bold">Progress <span class="float-end">100%</span></p>
-                            <div class="progress progress-sm">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                </div><!-- /.progress-bar -->
-                            </div><!-- /.progress -->
-                        </li>
-                    </ul>
-                </div> <!-- end card-->
-                </div> <!-- end col -->
-                @empty
-                <div class="card d-block">
-                    <div class="card-body">
-                        
+                    <div class="card-body position-relative">
                         <!-- project title-->
                         <h4 class="mt-0">
                             <a href="apps-projects-details.html" class="text-title">
-                                NO data Found
+                                {{$project->project_name}}
                             </a>
                         </h4>
-                    </div> 
+
+                        <!-- project detail-->
+                        <p class="mb-3">
+                            <span class="pe-2 text-nowrap">
+                                <i class="mdi mdi-format-list-bulleted-type"></i>
+                                <b>21</b> Tasks
+                            </span>
+                            <span class="text-nowrap">
+                                <i class="mdi mdi-comment-multiple-outline"></i>
+                                <b>668</b> Comments
+                            </span>
+                        </p>
+                        <div class="mb-3" id="tooltip-container6">
+                            <a href="javascript:void(0);" data-bs-container="#tooltip-container6" data-bs-toggle="tooltip" data-bs-placement="top" title="Mat Helme" class="d-inline-block">
+                                <img src="assets/images/users/avatar-6.jpg" class="rounded-circle avatar-xs" alt="friend">
+                            </a>
+
+                            <a href="javascript:void(0);" data-bs-container="#tooltip-container6" data-bs-toggle="tooltip" data-bs-placement="top" title="Michael Zenaty" class="d-inline-block">
+                                <img src="assets/images/users/avatar-7.jpg" class="rounded-circle avatar-xs" alt="friend">
+                            </a>
+
+                            <a href="javascript:void(0);" data-bs-container="#tooltip-container6" data-bs-toggle="tooltip" data-bs-placement="top" title="James Anderson" class="d-inline-block">
+                                <img src="assets/images/users/avatar-8.jpg" class="rounded-circle avatar-xs" alt="friend">
+                            </a>
+                            <a href="javascript:void(0);" class="d-inline-block text-muted fw-bold ms-2">
+                                +5 more
+                            </a>
+                        </div>
+
+                        <!-- project progress-->
+                        <p class="mb-2 fw-bold">Progress <span class="float-end">71%</span></p>
+                        <div class="progress progress-sm">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="71" aria-valuemin="0" aria-valuemax="100" style="width: 71%;">
+                            </div><!-- /.progress-bar -->
+                        </div><!-- /.progress -->
+                    </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end col -->
-                @endforelse
-                <!-- project card -->
-        </div>
-        <!-- end row-->
 
-        <!-- end row-->
+            @empty
+            <div class="card d-block">
+                <div class="card-body">
 
-    </div> <!-- End Content -->
-
-    <!-- Footer Start -->
-    <footer class="footer">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script> © Hyper - Coderthemes.com
+                    <!-- project title-->
+                    <h4 class="mt-0">
+                        <a href="apps-projects-details.html" class="text-title">
+                            NO data Found
+                        </a>
+                    </h4>
                 </div>
-                <div class="col-md-6">
-                    <div class="text-md-end footer-links d-none d-md-block">
-                        <a href="javascript: void(0);">About</a>
-                        <a href="javascript: void(0);">Support</a>
-                        <a href="javascript: void(0);">Contact Us</a>
-                    </div>
+            </div> <!-- end card-->
+        </div> <!-- end col -->
+
+
+        @endforelse
+
+
+        <!-- project card -->
+    </div>
+    <!-- end row-->
+
+    <!-- end row-->
+
+</div> <!-- End Content -->
+
+<!-- Footer Start -->
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> © Hyper - Coderthemes.com
+            </div>
+            <div class="col-md-6">
+                <div class="text-md-end footer-links d-none d-md-block">
+                    <a href="javascript: void(0);">About</a>
+                    <a href="javascript: void(0);">Support</a>
+                    <a href="javascript: void(0);">Contact Us</a>
                 </div>
             </div>
         </div>
-    </footer>
-    <!-- end Footer -->
+    </div>
+</footer>
+<!-- end Footer -->
 
 </div> <!-- content-page -->
 
