0bea84e0db1ed95fc76ee1bc5f4149f4feb417f1
Ashab Uddin <ashabuddin20@hotmail.com>
2022-11-19 23:19:00 +0600

    builder options

--- app/Http/Controllers/Builder/DesignController.php
+++ app/Http/Controllers/Builder/DesignController.php
@@ -14,6 +14,7 @@ class DesignController extends Controller
     public function __invoke(Request $request,$id)
     {
         $employee = User::where('role_id',3);
+        dd($employee);
         return view('Design.create',compact(['id','employee']));      
         
     }
