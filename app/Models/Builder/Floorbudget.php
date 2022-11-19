531c91217e1b5fb5a333a2bc17fda322af95017b
Md Jahidul Alam <jrony538>
2022-11-19 12:27:29 +0600

    floor budget

--- app/Models/Builder/Floorbudget.php
+++ app/Models/Builder/Floorbudget.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Floorbudget extends Model
+class FloorBudget extends Model
 {
     use HasFactory,SoftDeletes;
 
-    public function floorDetails(){
-        return $this->belongsTo(FloorDetails::class);
-    }
+    // public function floorDetail(){
+    //     return $this->belongsTo(FloorDetails::class);
+    // }
 }
