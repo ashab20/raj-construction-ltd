9f5c2ef08e4ea9898ad184f40cca04b1c19028ff
Rabib <jrony538>
2022-11-24 09:25:35 +0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,9 +34,9 @@ class DatabaseSeeder extends Seeder
             DivisionsSeeder::class,
             DistrictSeeder::class,
             ProjectSeeder::class,
+            LandsSeeders::class,
             FloorDetailsSeeder::class,
             UnitSeeder::class,
-            LandsSeeders::class,
         ]);
     }
 }
