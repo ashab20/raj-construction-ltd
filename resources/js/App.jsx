30e45ee3f9944b5293e0d3fe1590b24bd9bbc285
Md. Ariful Islam <hmarifrazi@github.com>
2022-12-07 13:30:18 +0600

    update

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,11 +1,19 @@
 
 import React from 'react';
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Header from './Components/Header';
+import Layout from './Components/Layout';
+import Main from './Components/Main';
 
 export default function App(){
     return(
-        <Header/>
+        <Router>
+            <Header/>
+            <Routes>
+                <Route path='react' element={<Layout><Main/></Layout>}/>
+            </Routes>
+        </Router>
     );
 }
 
