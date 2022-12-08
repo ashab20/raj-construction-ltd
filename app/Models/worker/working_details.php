61d5254c3f1c0ec6e073463e1bc66c53c588ad99
Md. Ariful Islam <hmarifrazi@github.com>
2022-12-08 11:01:49 +0600

    update

---
+++ app/Models/worker/working_details.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\worker;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class working_details extends Model
+{
+    use HasFactory;
+}
