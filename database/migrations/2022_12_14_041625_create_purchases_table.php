7caa726d7ad4b1d9ae3b9f8870df8a2685129caf
harunurR <harunurrashid97.ctg@gmail.com>
2022-12-14 11:00:36 +0600

    purchase

--- database/migrations/2022_12_14_041625_create_purchases_table.php
+++ database/migrations/2022_12_14_041625_create_purchases_table.php
@@ -17,12 +17,18 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->date('purchase_date');
-            $table->integer('total');
+            $table->string('voucher')->nullable();
             $table->integer('tax')->nullable()->default(0);
             $table->integer('discount')->nullable()->default(0);
-            $table->string('note',)->nullable()->default(0);
+            $table->integer('total_cost');
+            $table->integer('payment');
+            $table->string('note',1000)->nullable();
 
             $table->integer('status')->default(1);
+            
+            $table->unsignedBigInteger('purchase_by');
+            $table->foreign('purchase_by')->references('id')->on('users')->onDelete('cascade')->change();
+
             $table->unsignedBigInteger('created_by');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade')->change();
             
