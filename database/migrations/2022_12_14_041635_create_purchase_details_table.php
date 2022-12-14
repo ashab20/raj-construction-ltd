7cf12a0f47efe281f41e3e54f53ff0dba21ff92a
harunurR <harunurrashid97.ctg@gmail.com>
2022-12-14 11:18:35 +0600

    purches table done

--- database/migrations/2022_12_14_041635_create_purchase_details_table.php
+++ database/migrations/2022_12_14_041635_create_purchase_details_table.php
@@ -16,6 +16,31 @@ return new class extends Migration
         Schema::create('purchase_details', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->unsignedBigInteger('purchase_id');
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade')->change();
+            
+            $table->unsignedBigInteger('material_id');
+            $table->foreign('material_id')->references('id')->on('materials')->onDelete('cascade')->change();
+            
+            $table->integer('price');
+            $table->integer('discount')->nullable()->default(0);
+            $table->integer('tax')->nullable()->default(0);
+            $table->string('remarks');
+            $table->integer('quantitiy');
+            $table->integer('sub_total');
+            
+            $table->integer('status')->default(1);
+
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade')->change();
+
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade')->change();
+            
+            $table->softDeletes();
+           
+           
         });
     }
 
