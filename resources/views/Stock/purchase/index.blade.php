0eb8b4c49409890f1a448e90a5e6df9a860ea697
mdrabib <mdrabib@github.com>
2022-12-30 01:24:24 +0600

    r

--- resources/views/Stock/purchase/index.blade.php
+++ resources/views/Stock/purchase/index.blade.php
@@ -279,9 +279,7 @@
             console.log(data);
             content = `${data.purchase_date}`;
             $('#materialData').html(content);
-            content = `
-                    ${data.note}
-                `;
+            content = `${data.note}`;
 
             $('#materialnote').html(content);
 
